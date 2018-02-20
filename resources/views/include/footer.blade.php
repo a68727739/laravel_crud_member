77970965898ea184eb5958a11304862b6e97339b
henry chang <a68727739@gmail.com>
2018-02-21 01:08:08 +0800

    first

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,10 @@
+<footer id="footer" class="midnight-blue">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-6">
+				<p>&copy; 2017 All Rights Reserved. Powered by <a href="#">Henry Chang 張亨利</a></p>
+			</div>
+			
+		</div>
+	</div>
+</footer><!--/#footer-->
